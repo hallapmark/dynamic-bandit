882c7b9c247ec79945a0b3f0026b262bfc2c364f
Mark Hallap <hallapmark@gmail.com>
2024-06-18 10:26:58 -0400

    Initial commit

---
+++ bg/agent.py
@@ -0,0 +1,43 @@
+from numpy import random
+
+
+class Agent:
+    def __init__(self):
+        self.credence = random.uniform(0, 1)
+        self.k, self.n = 0, 0
+        self.did_experiment_this_round = False
+    
+    def __str__(self):
+        return f"credence = {round(self.credence, 2)}, k = {self.k}, n = {self.n}"
+
+    def experiment(self, n, epsilon):
+        if self.credence > .5:
+            self.k += random.binomial(n, .5 + epsilon)
+            self.n += n
+            self.did_experiment_this_round = True
+        else:
+            self.did_experiment_this_round = False
+            # exactly .5 or less: no longer experiments
+            # self.k, self.n = 0, 0
+
+    
+    def bayes_update(self, k, n, epsilon):
+        # self.credence = 1 / (1 + (1 - self.credence) * (((0.5 - epsilon) / (0.5 + epsilon)) ** (2 * k - n)) / self.credence)
+        self.credence = (k + 1)/(n + 2)
+    
+    # def jeffrey_update(self, neighbor, epsilon, m):
+    #     n = neighbor.n
+    #     k = neighbor.k
+        
+    #     p_E_H  = (0.5 + epsilon)**k * (0.5 - epsilon)**(n - k)         # P(E|H)  = p^k (1-p)^(n-k)
+    #     p_E_nH = (0.5 - epsilon)**k * (0.5 + epsilon)**(n - k)         # P(E|~H) = (1-p)^k p^(n-k)
+    #     p_E    = self.credence * p_E_H + (1 - self.credence) * p_E_nH  # P(E) = P(E|H) P(E) + P(E|~H) P(~H)
+        
+    #     p_H_E  = self.credence * p_E_H / p_E                           # P(H|E)  = P(H) P(E|H)  / P(E)
+    #     p_H_nE = self.credence * (1 - p_E_H) / (1 - p_E)               # P(H|~E) = P(H) P(~E|H) / P(~E)
+        
+    #     # q_E = max(1 - abs(self.credence - neighbor.credence) * m * (1 - p_E), 0)  # O&W's Eq. 1 (anti-updating)
+    #     q_E = 1 - min(1, abs(self.credence - neighbor.credence) * m) * (1 - p_E)    # O&W's Eq. 2
+
+    #     self.credence = p_H_E * q_E + p_H_nE * (1 - q_E)               # Jeffrey's Rule
+                                                                       # P'(H) = P(H|E) P'(E) + P(H|~E) P'(~E)
---
+++ zollman.py
@@ -0,0 +1,15 @@
+from bg.sim import process
+import timeit
+from multiprocessing import freeze_support
+
+if __name__ == '__main__':
+    freeze_support()
+    s = 10000
+    grid  = [(s, 'complete', a, 1000, .001, None, 1000) for a in range(4, 12)]
+    grid += [(s, 'cycle',    a, 1000, .001, None, 1000) for a in range(4, 12)]
+
+    tic = timeit.default_timer()
+    process(grid, 'results/zollman.csv')
+    toc = timeit.default_timer()
+
+    print("Time: " + str(round(toc - tic, 1)))
