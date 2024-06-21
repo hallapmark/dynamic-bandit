0f74f3e2c013a9a87f1d59fb79cc509aceee731c
Mark Hallap <hallapmark@gmail.com>
2024-06-21 17:28:51 -0400

    Revert "Add params model"
    This reverts commit d6c84b0020d7f065379a1a9945b18f0d806f9beb.

--- banditsim/sim.py
+++ banditsim/sim.py
@@ -5,14 +5,14 @@ from statistics import mean
 
 import numpy as np
 from banditsim.graph import Graph
-from banditsim.models import AnalyzedResults, ResultType, SimParams, SimResults
+from banditsim.models import AnalyzedResults, ResultType, SimResults
 
-def process(paramsgrid: list[SimParams], path):
-    for params in paramsgrid:
+def process(grid, path):
+    for params in grid:
         print(params)
-        graph, a, n, e, m, max_epochs = params.graph, params.a, params.n, params.e, params.m, params.max_epochs
+        n_simulations, graph, a, n, e, m, max_epochs = params
         pool = Pool()
-        results = pool.starmap(run_simulation, ((graph, a, n, e, m, max_epochs),) * params.n_simulations)
+        results = pool.starmap(run_simulation, ((graph, a, n, e, m, max_epochs),) * n_simulations)
         pool.close()
         pool.join()
         # for _ in range(n_simulations):
