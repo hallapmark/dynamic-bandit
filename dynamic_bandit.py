3596e0fa24c2f4ca36444321531c3ffb4ace5f92
Mark Hallap <hallapmark@gmail.com>
2024-11-16 20:29:54 -0500

    Prepare for recorded run

--- dynamic_bandit.py
+++ dynamic_bandit.py
@@ -6,28 +6,28 @@ from banditsim.sim import process
 
 if __name__ == '__main__':
     freeze_support()
-    n_simulations = 200
-    max_epochs = 4000
+    n_simulations = 500
+    max_epochs = 10000
 
     grid = [SimParams(GraphShape.COMPLETE, a, 50, sine_amp, sine_period, max_epochs, burn_in, epsilon,
              window_s, slow_updater_multiplier) 
-                                                                for a in (10,) # 4, 12
+                                                                for a in (5,10,) # 4, 12
                                                                 for sine_amp in (.1,)
                                                                 for sine_period in (1000,)
                                                                 for burn_in in (1,)
-                                                                for epsilon in (0,)
-                                                                for window_s in (None,) # 50, 100
+                                                                for epsilon in (0,0.05,0.1,0.15)
+                                                                for window_s in (None,50,100) # 50, 100
                                                                 for slow_updater_multiplier in (None,)
                                                                 ]
     
     grid += [SimParams(GraphShape.CYCLE, a, 50, sine_amp, sine_period, max_epochs, burn_in, epsilon, 
              window_s, slow_updater_multiplier)
-                                                                for a in (10,) # 4, 12
+                                                                for a in (5,10,) # 4, 12
                                                                 for sine_amp in (.1,)
                                                                 for sine_period in (1000,)
                                                                 for burn_in in (1,)
-                                                                for epsilon in (0,)
-                                                                for window_s in (None,)
+                                                                for epsilon in (0,0.05,0.1,0.15)
+                                                                for window_s in (None,50,100)
                                                                 for slow_updater_multiplier in (None,)
                                                                 ]
 
